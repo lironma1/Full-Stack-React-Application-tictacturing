4fdf0a889bf0c30167ea2106cb173935bc7a8a3a
liron maliker <kaskasof1@gmail.com>
2018-02-14 14:09:13 +0200

    Add files via upload

---
+++ src/config/enpoints.js
@@ -0,0 +1,1 @@
+export const relayApi = 'https://api.graph.cool/relay/v1/cjdii01s0198t01290kgkxdlb'
--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-//import registerServiceWorker from './registerServiceWorker'
+import {Router, browserHistory, applyRouterMiddleware} from 'react-router'
+import Routes from './routes'
+import Relay from 'react-relay/classic'
+import useRelay from 'react-router-relay'
+import {RelayNetworkLayer, urlMiddleware} from 'react-relay-network-layer'
+import {relayApi} from './config/enpoints'
+import auth from './utils/auth'
+
+const createHeaders = () => {
+  let idToken = auth.getToken()
+  if (idToken) {
+    return {
+      'Authorization': `Bearer ${idToken}`
+    }
+  } else {
+    return {}
+  }
+}
+
+Relay.injectNetworkLayer(
+  new RelayNetworkLayer([
+    urlMiddleware({
+      url: (req) => relayApi,
+    }),
+    next => req => {
+      req.headers = {
+        ...req.headers,
+        ...createHeaders()
+      }
+      return next(req)
+    },
+  ],{disableBatchQuery: true})
+)
 
 ReactDOM.render(
-  <div>
-    <h1>hello world</h1>
-  </div>,
-  document.getElementById('root'));
-// registerServiceWorker();
+  <Router
+    environment={Relay.Store}
+    render={applyRouterMiddleware(useRelay)}
+    history={browserHistory}
+    routes={Routes}
+  />,
+  document.getElementById('root')
+)
